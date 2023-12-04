f7b17dbd13ed751de9c93884769e3cd6e379ef91
Radost-Kostadinova <r.kostadinova2019@pmg-pernik.com>
2023-12-04 11:15:23 +0200

    zadanie 1

---
+++ WebShopAppMVC/WebShopApp.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+using WebShopAppMVC.Infrastructure.Data.Domain;
+
+namespace WebShopAppMVC.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Oders { get; set; }
+        public DbSet<Product> Products{ get; set; }
+
+    }
+}
---
+++ WebShopAppMVC/WebShopAppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebShopAppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
