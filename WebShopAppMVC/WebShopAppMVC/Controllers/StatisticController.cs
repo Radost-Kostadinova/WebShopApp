1eab37df630dadc187edc49f47eafc58e84521be
Radost-Kostadinova <r.kostadinova2019@pmg-pernik.com>
2024-01-08 11:10:39 +0200

    7 готово

---
+++ WebShopAppMVC/WebShopAppMVC/Controllers/StatisticController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WebShopApp.Core.Contracts;
+
+using WebShopAppMVC.Models.Statistic;
+
+namespace WebShopAppMVC.Controllers
+{
+    public class StatisticController: Controller
+    {
+
+        private readonly IStatisticService statisticsService;
+        public StatisticController(IStatisticService statisticsService)
+        {
+            this.statisticsService = statisticsService;
+
+        }
+
+        public IActionResult Index()
+        {
+            StatisticVM statistics = new StatisticVM();
+
+            statistics.CountClients = statisticsService.CountClients();
+            statistics.CountProducts = statisticsService.CountProducts();
+            statistics.CountOrders = statisticsService.CountOrders();
+            statistics.SumOrders = statisticsService.SumOrders();
+
+            return View(statistics);
+        }
+
+    }
+}
